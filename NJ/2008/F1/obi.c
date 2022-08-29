85bc3684e29570eeebb6a77a506069a92914aba6
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2022-08-29 20:15:24 -0300

    Exercícios do Nível Júnior - 2008 - Fase 1

---
+++ NJ/2008/F1/obi.c
@@ -0,0 +1,23 @@
+/*
+    OBI - NÍVEL JÚNIOR - 2008 - FASE 1 - OBI
+                                                */
+
+#include <stdio.h>
+
+int main () {
+    int N, P, X, Y;
+    int i, count = 0;
+
+    scanf("%d %d", &N, &P);
+
+    for (i = 0; i < N; i++) {
+        scanf("%d %d", &X, &Y);
+
+        if (X + Y >= P)
+            count++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
---
+++ NJ/2008/F1/telefone2.c
@@ -0,0 +1,73 @@
+/*
+    OBI - NÍVEL JÚNIOR - 2008 - FASE 1 - TELEFONE
+                                                */
+
+#include <stdio.h>
+
+int main () {
+    char telefone[16];
+    int i = 0;
+
+    scanf("%s", telefone);
+
+    while (telefone[i] != '\0') {
+        switch (telefone[i]) {
+            case 'A':
+            case 'B':
+            case 'C':
+                printf("2");
+                break;
+
+            case 'D':
+            case 'E':
+            case 'F':
+                printf("3");
+                break;
+
+            case 'G':
+            case 'H':
+            case 'I':
+                printf("4");
+                break;
+
+            case 'J':
+            case 'K':
+            case 'L':
+                printf("5");
+                break;
+
+            case 'M':
+            case 'N':
+            case 'O':
+                printf("6");
+                break;
+
+            case 'P':
+            case 'Q':
+            case 'R':
+            case 'S':
+                printf("7");
+                break;
+            
+            case 'T':
+            case 'U':
+            case 'V':
+                printf("8");
+                break;
+
+            case 'W':
+            case 'X':
+            case 'Y':
+                printf("9");
+                break;
+
+            default:
+                printf("-");
+                break;
+        }
+
+        i++;
+    }
+
+    return 0;
+}
